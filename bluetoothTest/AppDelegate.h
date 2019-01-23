af6bf752418db0b246b0bae455d45da0f7934e17
wpj <wangpengjieem@163.com>
2019-01-23 13:20:56 +0800

    Initial Commit

---
+++ bluetoothTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bluetoothTest
+//
+//  Created by apple on 2019/1/23.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bluetoothTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bluetoothTest
+//
+//  Created by apple on 2019/1/23.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
